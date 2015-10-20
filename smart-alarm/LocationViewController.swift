4757be88f6a0d20c3079116a687ce64a5c712370
Edward Wu <wuedward@bitbucket.org>
2015-10-20 14:46:00 -0400

    Added MapKit to LocationViewController

--- smart-alarm/LocationViewController.swift
+++ smart-alarm/LocationViewController.swift
@@ -7,9 +7,47 @@
 //
 
 import UIKit
+import MapKit
 
-class LocationViewController: UIViewController {
+class LocationViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    var annotation: MKAnnotation!
+    var localSearchRequest: MKLocalSearchRequest!
+    var localSearch: MKLocalSearch!
+    var localSearchResponse: MKLocalSearchResponse!
+    var error: NSError!
+    var pointAnnotation: MKPointAnnotation!
+    var pinAnnotationView: MKPinAnnotationView!
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        localSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = searchBar.text
+        localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.startWithCompletionHandler({
+            (localSearchResponse, error) -> Void in
+            if localSearchResponse == nil {
+                let alertController = UIAlertController(title: nil, message: "Address not found", preferredStyle: .Alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: .Default, handler: nil))
+                self.presentViewController(alertController, animated: true, completion: nil)
+                return
+            }
+            
+            self.pointAnnotation = MKPointAnnotation()
+            self.pointAnnotation.title = searchBar.text
+            self.pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude, longitude: localSearchResponse!.boundingRegion.center.longitude)
+            self.pinAnnotationView = MKPinAnnotationView(annotation: self.pointAnnotation, reuseIdentifier: nil)
+            self.mapView.centerCoordinate = self.pointAnnotation.coordinate
+            self.mapView.addAnnotation(self.pinAnnotationView.annotation!)
+            let regionRadius: CLLocationDistance = 1000
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(self.pointAnnotation.coordinate,
+                regionRadius * 2.0, regionRadius * 2.0)
+            self.mapView.setRegion(coordinateRegion, animated: true)
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
