c85b03d876df7d78b88bffc49fe46fd338af6d6e
Gideon I. Glass <gidglass@gmail.com>
2015-12-08 11:31:47 -0500

    Fixed reference to alarm notification sound

--- smart-alarm/smart-alarm/HTTP.swift
+++ smart-alarm/smart-alarm/HTTP.swift
@@ -12,90 +12,7 @@ import UIKit
 
 class HTTP: NSObject {
     
-    func get(url: String, getCompleted: (succeeded: Bool, msg: String) -> ()) {
-        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: {
-            (data, response, error) in
-            
-            // If no error occurred during server request
-            if error == nil {
-                // Parse the response
-                do {
-                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary
-                    if let parseJSON = json {
-                        let status = parseJSON["status"] as! NSString
-                        print(status as String)
-                        if (status as String == "Login successful!") {
-                            getCompleted(succeeded: true, msg:"Successful login")
-                        } else {
-                            getCompleted(succeeded: false, msg:"Failed login")
-                        }
-                    }
-                }
-                catch {
-                    print("Error parsing JSON")
-                }
-            }
-                
-            else {
-                print(error)
-                getCompleted(succeeded: false, msg: "Error occurred in get request")
-
-            }
-        })
-        
-        task.resume()
-    }
-    
-    func post (params: Dictionary<String,String>, url: String, postCompleted: (succeeded: Bool, msg: String) -> ()) {
-        // Set up request object
-        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
-        let session = NSURLSession.sharedSession()
-        request.HTTPMethod = "POST"
-        
-        do {
-            let body = ["user": params]
-            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(body, options: .PrettyPrinted)
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            
-            let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-                
-                do {
-                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary
-                    if let parseJSON = json {
-                        let status = parseJSON["status"] as! NSString
-                        if (status as String == "User created!") {
-                            postCompleted(succeeded: true, msg:"Successful registration")
-                        } else {
-                            postCompleted(succeeded: false, msg:status as String)
-                        }
-                    }
-                    else {
-                        print("Failed to find key")
-                        postCompleted(succeeded: false, msg:"Failed")
-                    }
-                }
-                    
-                catch {
-                    print("Failed to parse response")
-                    postCompleted(succeeded: false, msg:"Failed")
-                }
-                
-            })
-            task.resume()
-        }
-            
-        catch {
-            print("Failed to convert reponse to dictionary")
-            postCompleted(succeeded: false, msg:"Failed")
-        }
-        
-    }
-    
-    /* TESTING SIMPLER VERSION FOR BACKEND CREDIT */
+    /* HTTP POST REQUEST */
     
     func POST (url: String, requestJSON: NSData, postComplete: (success: Bool, msg: String) -> ()) {
         // Set up the request object
@@ -134,6 +51,8 @@ class HTTP: NSObject {
         })
         task.resume()
     }
+    
+    /* JSON CONVERSION */
     
     func toJSON (dict: NSDictionary) -> NSData? {
         if NSJSONSerialization.isValidJSONObject(dict) {
